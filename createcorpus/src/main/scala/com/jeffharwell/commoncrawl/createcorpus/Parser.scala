f5d980de2e1d0eec21680c175931da406237e03f
Jeff Harwell <jharwell@fuller.edu>
2017-01-20 21:53:02 -0800

    Initial implementation of the WET parser as an iterator

--- createcorpus/src/main/scala/com/jeffharwell/commoncrawl/createcorpus/Parser.scala
+++ createcorpus/src/main/scala/com/jeffharwell/commoncrawl/createcorpus/Parser.scala
@@ -2,31 +2,120 @@ package com.jeffharwell.commoncrawl.createcorpus
 
 import java.io.File;
 import java.io.BufferedInputStream
+import java.io.InputStream
 import java.io.FileInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.zip.GZIPInputStream
+import scala.collection.mutable.ListBuffer
+import scala.collection.immutable.List
 
 
-class Parser {
+class Parser(inputstream: InputStream) extends Iterator[List[String]] {
   val linebuffer = scala.collection.mutable.ArrayBuffer.empty[Byte]
   val recordbuffer = scala.collection.mutable.ArrayBuffer.empty[Byte]
   val newlinebyte = "\n".getBytes("UTF-8").last
+  var readbuffersize: Int = 1024
   var linecount: Int = 0
   var recordcount: Int = 0
   var recordlength: Int = 0
   var contentbytesread: Int = 0
   var bufferbytesread: Int = 0
   var printnextline: Boolean = false
+  var wetrecords: ListBuffer[String] = new ListBuffer()
+  var gzcInputStream: GZIPCompatibilityWrapper = new GZIPCompatibilityWrapper(inputstream)
+  var gzInputStream: GZIPInputStream = new GZIPInputStream(gzcInputStream)
+  var hasnext: Boolean = true
+
+  /* Here is a wrapper class that wraps an InputStream
+   * but always returns > 0 when .available() is called.
+   * This will cause GZIPInputStream to always make another 
+   * call to the InputStream to check for an additional 
+   * concatenated GZIP file in the stream.
+   */
+  class GZIPCompatibilityWrapper(inputstream: InputStream) extends InputStream {
+      val is: InputStream = inputstream
+
+      override def read(): Int = {
+          return(is.read());
+      }
+
+      override def read(b: Array[Byte]): Int = {
+          return(is.read(b));
+      }
+
+      override def read(b: Array[Byte], off: Int, len: Int): Int = {
+          return(is.read(b, off, len));
+      }
+
+      override def close(): Unit = {
+          is.close();
+      }
+
+      override def available(): Int = {
+          // Always say that we have 1 more byte in the
+          // buffer, even when we don't
+          val a: Int = is.available();
+          if (a == 0) {
+              return(1);
+          } else {
+              return(a);
+          }
+      }
+  }
+
+  /*
+   * This is the next() implementation for our Iterator
+   *
+   * @return A list of individual WET records
+   */
+  def next(): List[String] = {
+    var result = false
+    do {
+      result = this.parseBuffer
+    } while (!result)
+
+    return this.returnRecord() // will return an empty list if we hit end of stream
+  }
+
+  /*
+   * This is the hasNext() implementation for the iterator. The variable hasnext
+   * is set to false by the parseBuffer() method when we reach the end of the InputStream
+   *
+   * Note that this isn't entirely accurate. Because we are reading an InputStream on the fly
+   * we actually don't know if there is an entire record left or not, we won't know until we 
+   * try next() and actually hit the end of the stream. So. next() may return an empty list
+   * after .hasNext() has indicated that there is actually another record present.
+   *
+   * I suppose we could get around this by reading ahead one record ... hmm
+   *
+   * @return boolean indicating if there is another record in the iterator
+   */
+  def hasNext(): Boolean = {
+    return hasnext
+  }
+      
 
   /*
    * This is the method called by the program populating the a read buffer.
    * The read buffer will contain some small chunk of a WET file, the job of this
    * class is to take a sequence of those buffers and return a set of distinct records
    * from the WET file.
+   *
+   * @return true if we have found a complete record or reach end of stream, false otherwise
    */
-  def parseBuffer(buffer: Array[Byte], length: Int): Unit = {
-    //println("Called")
+
+  def parseBuffer(): Boolean = {
+    val buffer = new Array[ Byte ]( readbuffersize )
+    var length: Int = 0
+
+    length = gzInputStream.read(buffer)
+    if (length == -1) {
+      // We are done
+      hasnext = false
+      return true
+    }
+
     bufferbytesread = 0
     // pretty simple
     // the parseWetPayload and searchForCompleteLine methods will hand 
@@ -41,13 +130,39 @@ class Parser {
       // calling searchForCompleteLine
       searchForCompleteLine(buffer, length)
     }
+
+    // At this point we are done looking at the buffer, so go back and check
+    // the object data and see if we have any complete records
+    if (wetrecords.size > 0) {
+      // we have at least one complete record
+      return true
+    }
+    // no complete records
+    return false
   }
 
+  /*
+   * This method returns any complete WET records that we have thus far. If we don't have
+   * anything it will return an empty List
+   *
+   * @return List[String] where each string is a complete WET record
+   */
+  def returnRecord(): List[String] = {
+    // convert our buffer to a list
+    val toreturn: List[String] = wetrecords.toList
+    // clear our record buffer
+    wetrecords.clear
+    // return our list
+    return toreturn
+  }
 
   /* This method searchs the read buffer sequentially for complete lines of
    * text, when it finds one it calls checkLine(). If checkLine() sets the 
    * object variable recordlength then it will call parseWetPayload to 
    * process the payload.
+   *
+   * @param buffer the byte array to with bytes to process
+   * @param length the number of bytes to read from the buffer
    */
   def searchForCompleteLine(buffer: Array[Byte], length: Int): Unit = {
     var b = buffer(bufferbytesread)
@@ -79,18 +194,21 @@ class Parser {
    * Content-length header in actual octets (not UTF-8 bytes). If the payload is 
    * only part of the buffer then this method will call searchForCompleteLine so
    * that line by line processing of the WET file can continue.
+   *
+   * @param buffer the byte array to with bytes to process
+   * @param length the number of bytes to read from the buffer
    */
   def parseWetPayload(buffer: Array[Byte], length: Int): Unit = {
-    if (recordlength != 0 && (length - bufferbytesread <= recordlength - contentbytesread)) {
-      // if what is left in the buffer <= what is left in the record
+    if (recordlength != 0 && (length - bufferbytesread < recordlength - contentbytesread)) {
+      // if what is left in the buffer < what is left in the record
       // then grab the whole buffer
       //println("Copied buffer to recordbuffer: copied "+length+" bytes")
       recordbuffer.appendAll(buffer.slice(bufferbytesread,length))
       contentbytesread += length - bufferbytesread
-      // the buffer is empty, this should return to parseBuffer would will then return to the call
+      // the buffer is empty, this should return to parseBuffer which will then return to the call
       // usually for them to fill to buffer again
-    } else if (recordlength != 0 && (length - bufferbytesread > recordlength - contentbytesread)) {
-      // if what is left in the buffer > what is left in the record
+    } else if (recordlength != 0 && (length - bufferbytesread >= recordlength - contentbytesread)) {
+      // if what is left in the buffer >= what is left in the record
       // the grab what is left in the record and pass the rest to the searchForCompleteLine hunter
       //println("ContentBytesread "+contentbytesread+" length "+length+" recordlength "+recordlength+" bufferBytesRead "+bufferbytesread)
       var recordendinbuffer = bufferbytesread + (recordlength - contentbytesread) // this is where the payload end in the buffer
@@ -113,11 +231,13 @@ class Parser {
     }
   }
 
+  /*
+   * Handle the processing and recording of completed WET records when we find them
+   */
   def processRecord(): Unit = {
-    // need to implement this
-    //println("Start Content")
-    //println(new String(recordbuffer.toArray, "UTF-8"))
-    //println("End Content")
+    // We have a record, convert the bytes into a UTF-8 string and throw it in the
+    // ListBuffer that is holding our complete records
+    wetrecords += new String(recordbuffer.toArray, "UTF-8")
   }
 
   /* When we have extracted a complete line out of the read buffer
--- createcorpus/src/main/scala/com/jeffharwell/commoncrawl/createcorpus/main.scala
+++ createcorpus/src/main/scala/com/jeffharwell/commoncrawl/createcorpus/main.scala
@@ -1,24 +1,32 @@
 package com.jeffharwell.commoncrawl.createcorpus
 
 import com.typesafe.config.ConfigFactory
+import java.net.URL
 import java.io.File
 import java.util.zip.GZIPInputStream
 import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.io.FileInputStream
 
 object Main extends App {
   val conf = ConfigFactory.load()
+
+  /*
+   * Try with the local WET file
+   */
   val testwetfilename = conf.getString("general.testwetfile")
 
   val gzipFile: File = getTheFile(testwetfilename)
 
-  val gzInputStream = new GZIPInputStream(new BufferedInputStream(new FileInputStream(gzipFile)))
-  val buffer = new Array[ Byte ]( 1024 )
-  val parser = new Parser()
+  var records: Int = 0;
+  val parser = new Parser(new BufferedInputStream(new FileInputStream(gzipFile)))
 
-  Stream.continually(gzInputStream.read(buffer)).takeWhile(_ != -1).foreach(parser.parseBuffer(buffer, _))
-  println("Saw "+parser.getLineCount()+" lines")
-  println("Saw "+parser.getRecordCount()+" records")
+  // Count the Records
+  records = parser.foldLeft(0) { (acc, i) =>
+    acc + 1
+  }
+  println(s"Found $records")
 
   // Little function to get the full path of the file in resources or die trying
   def getTheFile(testwetfilename: String): File = {
@@ -29,5 +37,4 @@ object Main extends App {
       case None    => { throw new RuntimeException(s"Didn't find a resource $testwetfilename") }
     }
   }
-
 }
